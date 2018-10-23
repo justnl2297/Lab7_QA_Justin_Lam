c7e1759209f3ee60c647e7c7b59f258cd58f0633
Justin Lam <tug01225@temple.edu>
2018-10-23 17:58:48 -0400

    first commit for Lab7

---
+++ Lab7_TicTacToe/Lab7_TicTacToe/TicTacToeClass.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab7_TicTacToe
+{
+    public class TicTacToeClass
+    {
+        public string currentPlayer;
+        public int turn;
+
+        //array of bool indexes to check if user is an X = false , or O = true
+        public string[] board = new string[9];
+
+        //fills an array of empty spaces and determines the start player for the match
+        public void CreateBoard()
+        {
+            //for loop is to traverse through the array and make all the spaces empty and ready for play. 
+            for (int i = 0; i < board.Length - 1; i++)
+            {
+                board[i] = "placement";  //the, "placement" word means that the space is usable on the board.
+            }
+
+         
+
+        }
+       //decides who plays first
+        public string CurrentPlayer()
+        {
+
+            //generates random number 1 or 2 to decide if player 0(O) or 1(X) starts. 
+            turn = 0; //X WILL START FIRST
+            if (turn == 0)
+            {
+                currentPlayer = "X";
+
+            }
+            else if (turn == 1)
+            {
+                currentPlayer = "O";
+            }
+
+            return currentPlayer; 
+
+        }
+        //incraments the XorO variable in order to change players.
+        public string ChangePlayer(int turn)
+        {
+            turn++;
+            //mod 2 because remainder will always be 0 or 1 and this is a good way to switch between the next turn.
+            if (turn % 2 == 0)
+            {
+                currentPlayer = "O";
+            }
+            else if (turn % 2 == 1)
+            {
+                currentPlayer = "X";
+            }
+
+            return currentPlayer; 
+        }
+        //Use this method by make a move by ID (0 through 8 since there are 9 spaces) 
+        public string MakeMove(int moveID, int turn)
+        {
+            if (turn % 2 == 0)
+            {
+                board[moveID] = "O";
+            }
+            else if (turn % 2 == 1)
+            {
+                board[moveID] = "X";
+            }
+            return board[moveID]; //returning move value for the unit test.
+
+        }
+        //method checks the array index, if it contains the string placement then it is usable and returns a true. 
+        public bool SpaceInUse(int moveID)
+        {
+            if (board[moveID] == "placement")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ Lab7_TicTacToe/Lab7_TicTacToeTests/TicTacToeTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Lab7_TicTacToe;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Lab7_TicTacToe.Tests
+{
+    [TestClass()]
+    public class TicTacToeTests
+    {
+        TicTacToeClass game = new TicTacToeClass();
+        [TestMethod()]
+        public void checkBoardSizeTest()
+        {
+
+            const int boardSize = 9;
+            game.CreateBoard();
+            Assert.AreEqual(boardSize, game.board.Length);
+        }
+        [TestMethod()]
+        public void checkifPlayerisXTest()
+        {
+            const string playerX = "X";
+            Assert.AreEqual(playerX, game.CurrentPlayer());
+        }
+
+        [TestMethod()]
+        public void checkifPlayerisOTest()
+        {
+            const string playerO = "O";
+             int turn = 1; // turns will incrament in the TicTacToeClass so we will use #1 for an example of showing that it is player O's turn.
+            Assert.AreEqual(playerO, game.ChangePlayer(turn));
+        }
+        [TestMethod()]
+        public void xIsPlacedTest()
+        {
+            const string playerMove = "X";
+            int turn = 1;
+            Assert.AreEqual(playerMove, game.MakeMove(1,turn));//made it O's turn to make a placement, and used a random moveID to fufill the method parameters.
+        }
+
+        [TestMethod()]
+        public void oIsPlacedTest()
+        {
+            const string playerMove = "O";
+            int turn = 0;
+            Assert.AreEqual(playerMove, game.MakeMove(1, turn));//made it O's turn to make a placement, and used a random moveID to fufill the method parameters.
+        }
+        [TestMethod()]
+        public void emptySpaceTest()
+        {
+            bool spaceIs = true; // if true, then space is placeable(placement)
+            game.CreateBoard(); //method must run in order to create the empty placement spots in the array(board). 
+            Assert.AreEqual(spaceIs, game.SpaceInUse(1)); // game from the start already sets all spaces empty so any moveID will be placement without the game being played.
+        }
+    }
+}
